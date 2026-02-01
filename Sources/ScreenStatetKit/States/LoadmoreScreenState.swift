0b51d0ba5557b005fd05719c9591f1cde0fbec12
Thang Kieu <thanglk.work@gmail.com>
2026-02-01 14:49:46 +0700

    Just clean code

--- Sources/ScreenStatetKit/States/LoadmoreScreenState.swift
+++ Sources/ScreenStatetKit/States/LoadmoreScreenState.swift
@@ -1,5 +1,5 @@
 //
-//  LoadmoreScreenStates.swift
+//  LoadmoreScreenState.swift
 //  ScreenStatetKit
 //
 //  Created by Anthony on 4/12/25.
@@ -10,7 +10,7 @@ import Combine
 //MARK: - Loadmore Screen States
 
 @Observable
-open class LoadmoreScreenStates: ScreenState {
+open class LoadmoreScreenState: ScreenState {
     
     public private(set) var canShowLoadmore: Bool = false
     public private(set) var didLoadAllData: Bool = false
